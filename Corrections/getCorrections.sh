c2ada91bc065ce1069c402cac9d0a1fe7a091c6a
Austin <abaty@mit.edu>
2016-01-25 08:54:22 -0500

    small change to getCOrrections macro

--- Corrections/getCorrections.sh
+++ Corrections/getCorrections.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then
+    echo $0: usage: script isPP
+    exit 1
+fi
+
+isPP=""
+if [ "$1" = "pp" ]; then
+  isPP=$1
+fi
+inDir="/mnt/hadoop/cms/store/user/abaty/temporaryStorage/"
+inFile="*Hists_job*.root"
+
 echo copying histograms over
 echo make sure to copy the TrkCorrInputFile.txt and weights file over as well!
-mv /mnt/hadoop/cms/store/user/abaty/temporaryStorage/*Hists_job*.root . 
+mv $inDir$isPP$inFile . 
